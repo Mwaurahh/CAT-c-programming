dc7e8bda32ee7273b542b8ab2584a67265fc0cc5
Mwaurahh <mwaurasamson82@gmail.com>
2024-11-07 12:11:19 +0000

    fix code indentation

--- CAT2_Q3.c
+++ CAT2_Q3.c
@@ -1,38 +1,33 @@
 #include <stdio.h>
 
-int main()
-{
-	int hours, overTime;
-	float wage, grossPay,tax, netPay,increment;
-	
-	
-	
-	printf("enter hours worked in a week:");
-	scanf("%d",&hours);
-	printf("enter hourly wage:");
-	scanf("%f",&wage);
-	
-if (hours<41){
-	grossPay=hours*wage;
-	
-}
-else{
-	overTime = (hours-40);
-	increment = overTime*(1.5*wage);
-	grossPay=(40*wage)+increment;
+int main() {
+    int hours, overTime;
+    float wage, grossPay, tax, netPay, increment;
 
-}
-	if(grossPay<601){
-	tax=grossPay*0.15;
-	}
-	else 
-	{
-		tax=grossPay*0.2;
-	} 
-	netPay = grossPay - tax;
-	
-	printf("grossPay :%.2f\n",grossPay);
-	printf("tax is :%.2f\n",tax);
-	printf("netpay: %.2f\n",netPay);
-	return 0;
+    printf("Enter hours worked in a week: ");
+    scanf("%d", &hours);
+    printf("Enter hourly wage: ");
+    scanf("%f", &wage);
+
+    if (hours < 41) {
+        grossPay = hours * wage;
+    } else {
+        overTime = (hours - 40);
+        increment = overTime * (1.5 * wage);
+        grossPay = (40 * wage) + increment;
+    }
+
+    if (grossPay < 601) {
+        tax = grossPay * 0.15;
+    } else {
+        tax = grossPay * 0.20;
+    }
+
+    netPay = grossPay - tax;
+
+    printf("Gross Pay: %.2f\n", grossPay);
+    printf("Tax: %.2f\n", tax);
+    printf("Net Pay: %.2f\n", netPay);
+
+    return 0;
 }
